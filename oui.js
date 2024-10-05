5a5494ba9a79a66a7e533a968b8117a7055bb5e8
tobio <lucasrevault32@gmail.com>
2024-10-05 16:14:08 +0200

    page accueil plus complete

--- oui.js
+++ oui.js
@@ -1,8 +1,22 @@
-// Select the burger menu and the navigation elements
+// Sélection du menu burger et du menu latéral
 const burgerMenu = document.querySelector('.burger-menu img');
-const navMenu = document.querySelector('nav ul');
+const sideMenu = document.querySelector('.side-menu');
+const sideBurgerMenu = document.querySelector('.side-burger-menu');
 
-// Toggle the visibility of the navigation menu on burger menu click
-burgerMenu.addEventListener('click', () => {
-    navMenu.classList.toggle('active');
+// Ajout d'un événement pour le bouton du burger
+document.getElementById("burger-btn").addEventListener("click", function() {
+    var menu = document.getElementById("side-menu");
+
+    // Toggle active class pour afficher ou cacher le menu
+    if (menu.classList.contains("active")) {
+        menu.classList.remove("active");
+
+        // Remettre le bouton du menu burger à sa position d'origine
+        sideBurgerMenu.style.top = "120px";  // Position initiale sous le logo
+    } else {
+        menu.classList.add("active");
+
+        // Repositionner le bouton du menu burger vers le haut
+        sideBurgerMenu.style.top = "20px";  // Position en haut lors de l'ouverture
+    }
 });
